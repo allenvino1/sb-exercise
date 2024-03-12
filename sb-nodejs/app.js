b3dc50c509bc222e8eda313827556f0febe636e9
Allen Baldovino <baldovino@mclinica.com>
2024-03-12 10:38:04 +0800

    first commit

---
+++ sb-nodejs/app.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const app = express();
+
+
+const ENVIRONMENT = process.env.ENVIRONMENT || "production";
+
+app.get('/', (req, res) => {
+   res.send(`Environment: ${ENVIRONMENT}`);
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
